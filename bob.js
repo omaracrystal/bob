29009a4481d02d8316f2be69fb38825bc058efef
Crystal <crystal@Zachs-MacBook-Air.local>
2015-10-28 10:39:34 -0600

    exercism exercise

---
+++ bob.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+var Bob = function() {
+
+};
+
+Bob.prototype.hey = function(input) {
+  if (input.trim() === "") {
+    return "Fine. Be that way!";
+  }  else if (input === input.toUpperCase() && input.match(/[A-Z]/g)) {
+    return "Whoa, chill out!";
+  } else if (input.slice(-1) === '?') {
+    return "Sure.";
+  } else {
+    return "Whatever.";
+  }
+};
+
+module.exports = Bob;
